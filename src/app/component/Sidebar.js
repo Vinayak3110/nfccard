c23a3d0d933b6c2d42a148712c2aa2e5a0debebc
Vinayak3110 <107124477+Vinayak3110@users.noreply.github.com>
2025-01-10 10:51:53 +0530

    Update Sidebar.js
    Vinayak Sharma Changed For DE Project

--- src/app/component/Sidebar.js
+++ src/app/component/Sidebar.js
@@ -1,38 +1,53 @@
 import React from 'react';
 import { Home, CreditCard, Wallet, Users, FileText, BarChart } from 'lucide-react';
-
 const Sidebar = () => {
   return (
     <div className="w-64 h-screen bg-gray-200 p-4">
       <h1 className="font-bold text-xl mb-8">FinBank</h1>
+      <h1 className="font-bold text-xl mb-8">Uni-Card</h1>
       <ul className="grid gap-6">
         <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg ">
           <Home className="w-5 h-5" />
           <span>Dashboard</span>
+          <span>Profile</span>
         </li>
         <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
+          <CreditCard className="w-5 h-5" />
+          <span>Transactions</span>
+
+
+<li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg ">
+          <Home className="w-5 h-5" />
+          <span>Attendance</span>
+        </li>
+        <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
+          <Wallet className="w-5 h-5" />
+    <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg ">
+          <Home className="w-5 h-5" />
+          <span>Card Center</span>
+        </li>
+    <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg ">
+          <Home className="w-5 h-5" />
+          <span>Announcement</span>
+        </li>
+        <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
+          <Users className="w-5 h-5" />
+          <span>Contacts</span>
           <CreditCard className="w-5 h-5" />
           <span>Transactions</span>
         </li>
         <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
           <Wallet className="w-5 h-5" />
-          <span>Card Center</span>
-        </li>
-        <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
-          <Users className="w-5 h-5" />
-          <span>Contacts</span>
-        </li>
-        <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
-          <Wallet className="w-5 h-5" />
           <span>E-Wallet Center</span>
+          <span>Report</span>
         </li>
         <li className="flex items-center space-x-3 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
           <BarChart className="w-5 h-5" />
           <span>Reports</span>
+          <Users className="w-5 h-5" />
+          <span>Setting</span>
         </li>
+
       </ul>
     </div>
   );
-};
-
-export default Sidebar;
